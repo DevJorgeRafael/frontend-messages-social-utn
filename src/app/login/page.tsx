b67c9da0df099291e56071534c0c5300fac674a8
Jorge Rafael Rosero <rafaelrosero.r.r2408@gmail.com>
2024-06-24 12:03:52 -0500

    config the project for login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useState } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { SubmitHandler, useForm } from "react-hook-form";
 
 type FormData = {
@@ -16,6 +16,46 @@ export default function Login() {
         console.log(data)
 
         //Lógica de autenticación
-        router.push('main')
+        router.push('/main')
     }
+
+    return (
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+        <div className="bg-white p-8 rounded shadow-md w-full max-w-sm">
+          <h1 className="text-xl mb-4 text-gray-900">Iniciar Sesión</h1>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="mb-4">
+              <label className="block mb-2 text-gray-800">Usuario</label>
+              <input
+                {...register("username", { required: "El usuario es obligatorio" })}
+                type="text"
+                placeholder="Usuario"
+                className="p-2 border border-gray-300 rounded w-full text-gray-800"
+              />
+              {errors.username && (
+                <p className="text-red-500">{errors.username.message}</p>
+              )}
+            </div>
+            <div className="mb-6">
+              <label className="block mb-2 text-gray-800">Contraseña</label>
+              <input
+                {...register("password", { required: "La contraseña es oblogatoria" })}
+                type="password"
+                placeholder="Contraseña"
+                className="p-2 border border-gray-300 rounded w-full text-gray-800"
+              />
+              {errors.password && (
+                <p className="text-red-500">{errors.password.message}</p>
+              )}
+            </div>
+            <button
+              type="submit"
+              className="bg-blue-500 text-white p-2 rounded w-full"
+            >
+              Iniciar Sesión
+            </button>
+          </form>
+        </div>
+      </div>
+    );
 }
