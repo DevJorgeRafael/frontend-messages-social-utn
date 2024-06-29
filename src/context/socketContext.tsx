72855b9868559fce4374806a269f14336785f8ff
Jorge Rafael Rosero <rafaelrosero.r.r2408@gmail.com>
2024-06-28 23:42:34 -0500

    improving the socketContext

--- src/context/socketContext.tsx
+++ src/context/socketContext.tsx
@@ -29,15 +29,34 @@ export const SocketProvider = ({ children }: { children: ReactNode }) => {
 
   useEffect(() => {
     if (isAuthenticated && !socket) {
-      const newSocket = io("http://localhost:3000"); // Asegúrate de que esta URL sea la correcta
+      const newSocket = io("http://localhost:3000", {
+        path: "/socket.io",
+        withCredentials: true,
+        forceNew: true,
+        reconnectionAttempts: 3,
+        timeout: 2000,
+        transports: ["websocket", "polling"],
+      });
       setSocket(newSocket);
 
       newSocket.on("connect", () => {
-        console.log("Connected to WebSocket server");
+        console.log("Connected to server");
       });
 
-      newSocket.on("disconnect", () => {
-        console.log("Disconnected from WebSocket server");
+      newSocket.on("disconnect", (reason) => {
+        console.log("Disconnected from server:", reason);
+      });
+
+      newSocket.on("connect_error", (error) => {
+        console.error("Connection error:", error);
+      });
+
+      newSocket.on("connect_timeout", (timeout) => {
+        console.error("Connection timeout:", timeout);
+      });
+
+      newSocket.on("reconnect_attempt", (attemptNumber) => {
+        console.log("Reconnection attempt:", attemptNumber);
       });
 
       return () => {
